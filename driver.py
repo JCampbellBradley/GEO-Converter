03ac710c8e7e5df2991d12eb6b821bc016c499c2
Campbell Bradley <jcampbellbradley@gmail.com>
2024-08-23 14:44:25 -0600

    Removed extraneous files

--- driver.py
+++
@@ -1,8 +0,0 @@
-from neo4j import GraphDatabase
-
-
-uri = "bolt://localhost:7474"
-with GraphDatabase.driver(uri, auth=("neo4j", "password")) as driver:
-	with driver.session() as session:
-		result = session.run("MATCH (n) return n;")
-		print(result)
--- util.py
+++
@@ -1,5 +0,0 @@
-def encode_commas(s):
-	return s.replace(",", "[COMMA]").replace(";", "[SEMICOLON]")
-
-def decode_commas(s):
-	return f"replace(replace(replace({s}, ';', '\t'), '[COMMA]', ','), '[SEMICOLON]', ';')"
